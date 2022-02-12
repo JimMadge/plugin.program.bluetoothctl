7eeea7e337f0d2ca4f0931be027ec2d4370ccacb
Jim Madge <jim@jmadge.com>
2022-02-12 14:23:33 +0000

    Move Endpoint class to a new module

---
+++ addon/endpoints.py
@@ -0,0 +1,70 @@
+import urllib.parse
+
+
+class Endpoints:
+    def __init__(self, base_url: str):
+        """
+        Object which constructs endpoints for the various modes of the plugin.
+
+        Args:
+            base_url: The FQDN on the plugin
+        """
+        self.base_url = base_url
+
+    def build_url(self, query: dict[str, str]) -> str:
+        """
+        Construct a url to recall this script with a set of arguments encoded
+        in the query string.
+
+        Args:
+            query: Dict of arguments
+        """
+        return ''.join([self.base_url, '?', urllib.parse.urlencode(query)])
+
+    def build_url_connect(self, device: str, address: str) -> str:
+        """
+        Construct a url for the connect entry point.
+
+        Args:
+            device: Name of device
+            address: Address of device
+        """
+        return self.build_url(
+            {'mode': 'connect', 'device': device, 'address': address}
+        )
+
+    def build_url_disconnect(self, device: str, address: str) -> str:
+        """
+        Construct a url for the disconnect entry point.
+
+        Args:
+            device: Name of device
+            address: Address of device
+        """
+        return self.build_url(
+            {'mode': 'disconnect', 'device': device, 'address': address}
+        )
+
+    def build_url_pair(self, device: str, address: str) -> str:
+        """
+        Construct a url for the pair entry point.
+
+        Args:
+            device: Name of device
+            address: Address of device
+        """
+        return self.build_url(
+            {'mode': 'pair', 'device': device, 'address': address}
+        )
+
+    def build_url_remove(self, device: str, address: str) -> str:
+        """
+        Construct a url for the remove entry point.
+
+        Args:
+            device: Name of device
+            address: Address of device
+        """
+        return self.build_url(
+            {'mode': 'remove', 'device': device, 'address': address}
+        )
--- addon/main.py
+++ addon/main.py
@@ -1,12 +1,12 @@
 from collections.abc import Generator
 from contextlib import contextmanager
-import urllib.parse
 import xbmc  # type: ignore
 import xbmcaddon  # type: ignore
 import xbmcgui  # type: ignore
 import xbmcplugin  # type: ignore
-from addon.bluetoothctl import Bluetoothctl
-from addon.logging import loginfo
+from .bluetoothctl import Bluetoothctl
+from .endpoints import Endpoints
+from .logging import loginfo
 
 
 @contextmanager
@@ -19,75 +19,6 @@ def busy_dialog() -> Generator[None, None, None]:
         yield
     finally:
         xbmc.executebuiltin('Dialog.Close(busydialognocancel)')
-
-
-class Endpoints:
-    def __init__(self, base_url: str):
-        """
-        Object which constructs endpoints for the various modes of the plugin.
-
-        Args:
-            base_url: The FQDN on the plugin
-        """
-        self.base_url = base_url
-
-    def build_url(self, query: dict[str, str]) -> str:
-        """
-        Construct a url to recall this script with a set of arguments encoded
-        in the query string.
-
-        Args:
-            query: Dict of arguments
-        """
-        return ''.join([self.base_url, '?', urllib.parse.urlencode(query)])
-
-    def build_url_connect(self, device: str, address: str) -> str:
-        """
-        Construct a url for the connect entry point.
-
-        Args:
-            device: Name of device
-            address: Address of device
-        """
-        return self.build_url(
-            {'mode': 'connect', 'device': device, 'address': address}
-        )
-
-    def build_url_disconnect(self, device: str, address: str) -> str:
-        """
-        Construct a url for the disconnect entry point.
-
-        Args:
-            device: Name of device
-            address: Address of device
-        """
-        return self.build_url(
-            {'mode': 'disconnect', 'device': device, 'address': address}
-        )
-
-    def build_url_pair(self, device: str, address: str) -> str:
-        """
-        Construct a url for the pair entry point.
-
-        Args:
-            device: Name of device
-            address: Address of device
-        """
-        return self.build_url(
-            {'mode': 'pair', 'device': device, 'address': address}
-        )
-
-    def build_url_remove(self, device: str, address: str) -> str:
-        """
-        Construct a url for the remove entry point.
-
-        Args:
-            device: Name of device
-            address: Address of device
-        """
-        return self.build_url(
-            {'mode': 'remove', 'device': device, 'address': address}
-        )
 
 
 def main(base_url: str, addon_handle: str, args: dict[str, str]):
